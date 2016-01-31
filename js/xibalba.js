24e995dece11568f598b4dcd0dc8b674cc0bb05e
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-31 14:41:11 -0600

    Empty Phaser project

---
+++ js/xibalba.js
@@ -0,0 +1,28 @@
+var game = new Phaser.Game(
+  300,
+  600,
+  Phaser.CANVAS,
+  'gameDiv',
+  {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+  }
+);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
