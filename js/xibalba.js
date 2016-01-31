8c168703bffee68144c0344d3d66ff200b11e8a1
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-31 15:42:27 -0600

    Show player arms

--- js/xibalba.js
+++ js/xibalba.js
@@ -14,15 +14,31 @@ var game = new Phaser.Game(
 function preload() {
   game.load.image('background', 'assets/background.png');
   game.load.image('playerBody', 'assets/playerBody.png');
+  game.load.image('leftForearm', 'assets/leftForearm.png');
+  game.load.image('rightForearm', 'assets/rightForearm.png');
 }
 
 function create() {
   bg = game.add.tileSprite(0, 0, 400, 800, 'background');
-  bg.scale.setTo(.75)
+  bg.scale.setTo(.75);
 
-  playerBody = game.add.sprite(game.world.centerX, 575, 'playerBody')
-  playerBody.scale.setTo(.75)
+  player = game.add.group();
+  player.scale.setTo(.75);
+
+  playerBody = game.add.sprite(200, 777, 'playerBody');
   playerBody.anchor.setTo(0.5, 0.5);
+
+  leftForearm = game.add.sprite(170, 763, 'leftForearm');
+  leftForearm.anchor.setTo(0.5, 1.0);
+  leftForearm.angle = -45;
+
+  rightForearm = game.add.sprite(239, 755, 'rightForearm');
+  rightForearm.anchor.setTo(0.5, 0);
+  rightForearm.angle = 45;
+
+  player.add(playerBody);
+  player.add(leftForearm);
+  player.add(rightForearm);
 }
 
 function update() {
