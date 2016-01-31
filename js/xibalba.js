5a9e6f797f94a248f3ec07c40d7bc56ae6e0211e
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-31 15:13:29 -0600

    Show player body

--- js/xibalba.js
+++ js/xibalba.js
@@ -13,11 +13,15 @@ var game = new Phaser.Game(
 
 function preload() {
   game.load.image('background', 'assets/background.png');
+  game.load.image('playerBody', 'assets/playerBody.png');
 }
 
 function create() {
   bg = game.add.tileSprite(0, 0, 400, 800, 'background');
   bg.scale.setTo(.75)
+
+  playerBody = game.add.sprite(game.world.centerX, 575, 'playerBody')
+  playerBody.anchor.setTo(0.5, 0.5);
 }
 
 function update() {
